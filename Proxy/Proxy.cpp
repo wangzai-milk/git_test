779d0082c59d4119d2a1b9526f5f4b02f118abf5
wangzai-milk <865580876@qq.com>
2022-11-23 22:56:18 +0800

    测试代码

---
+++ Proxy/Proxy.cpp
@@ -0,0 +1,14 @@
+// Proxy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+    std::string name;         // define name
+    std::cin >> name;
+    std::cout << name.c_str() << std::endl;
+}
+ 
